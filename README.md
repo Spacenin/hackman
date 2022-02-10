98015e49b7ab3818bb259e4afc01284a4a7e52c9
Spacenin <chepstudios@gmail.com>
2022-02-10 09:26:38 -0500

    Initialized virtual env

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 Hangman game and project created for the Clemson CPSC Discord Hackman challenge!
 
 Attributions:
-1. [H Logo](https://www.flaticon.com/free-icons/alphabet)
+1. [H Logo](https://www.flaticon.com/premium-icon/letter-h_3097026?term=h&page=1&position=7&page=1&position=7&related_id=3097026&origin=search)
--- game.py
+++ game.py
@@ -1,18 +1,35 @@
 import PyQt6.QtWidgets as qtw
 import PyQt6.QtGui as qtg
+import getWord
 
 #Game window class 
 class game(qtw.QWidget):
     def __init__(self):
+        #Set up member variables
+        self.status = -1
+        self.word = getWord.getWord()
+
         super().__init__()
 
         #Set size, title, and other properties
-        self.setGeometry(0, 0, 500, 600)
+        self.setGeometry(0, 0, 1000, 600)
         self.setWindowTitle("Hackman!")
         self.setWindowIcon(qtg.QIcon("assets/letter-h.png"))
-        self.setLayout(qtw.QGridLayout())
+        self.layout = qtw.QVBoxLayout()
         self.setStyleSheet("background-color: black;")
 
+        self.draw()
+    
+    def draw(self):
+        if self.status == -1:
+            label = qtw.QLabel(self)
+            pxmap = qtg.QPixmap("assets/base.png")
+            label.setPixmap(pxmap)
+            label.setMaximumHeight(pxmap.height())
+            label.setMaximumWidth(pxmap.width())
+            self.layout.addWidget(label)
+
         self.show()
+
 
     
